902da14f13d3344238b11ab0cae105d070959ae7
Sameerkendal <kendalsameer@gmail.com>
2023-08-09 13:37:33 +0530

    uploaded

--- codes/assign.py
+++ codes/assign.py
@@ -3,23 +3,24 @@ import numpy as np
 A= np.array([[1],[-1]])
 B= np.array([[-4],[6]])
 C=np.array([[-3],[-5]])
+T=np.array([[0,1],[-1,0]])
 # finding  the direction vector for the lines BC AND CA
 m1= C-B
 m2= A-C
-# finding the n(transpose) vector such that nT*m =0
-n2T= np.array([-m1[1][0],m1[0][0]])
-n3T=np.array([m2[1][0],-m2[0][0]])
+# finding the nvector such that nT*m =0
+n2= np.dot(T,m1)
+n3=np.dot(T,m2)
+n2T= np.transpose(n2)
+n3T= np.transpose(n3)
 # finding the constant value in normal form of eqaution
-c2= n2T@B
-c3= n3T@C
-n2r= np.transpose(n2T)
-n3r= np.transpose(n3T)
-#norm of n2 and n3
-norm2=math.sqrt(n2r@n2T)
-norm3=math.sqrt(n3r@n3T)
+c2= np.dot(n2T,B)
+c3= np.dot(n3T,C)
+#norm of n2 and n3,
+norm2=np.linalg.norm(n2)
+norm3=np.linalg.norm(n3)
 # angle bisector equation
-nfT= (n2T/norm2)-(n3T/norm3)
-cfT= (c2/norm2)-(c3/norm3)
+nfT= (n2T/norm2)+(n3T/norm3)
+cfT= (c2/norm2)+(c3/norm3)
 print("The Equation of angular bisector of angle C is ",nfT,"X = ",cfT)
 
 import sys
